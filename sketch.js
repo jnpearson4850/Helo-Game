a668cdaa4d1f531ee9609c7d8377b00615a2477f
Joshua Nicholas Pearson <jnpearson4850@gmail.com>
2018-09-12 13:19:14 -0400

    adding objs

--- sketch.js
+++ sketch.js
@@ -1,38 +1,42 @@
 var player;
-var walls;
-
-
 // background image
 var img;
+var pipes;
 
+//timer
 function preload() {
     imgImg = loadImage("https://i.imgur.com/aKQOg3G.png");
+
 }
 function setup() {
-    createCanvas(5000, 700);
-    player = createSprite(100, 400, 100, 100);
-    walls = createSprite(50, 50, 50, 100)
-    for (var i = 0; i < 50; i++) {
-        walls.push(new Roadblocks())
-    }
+    createCanvas(1000, 700);
+    player = createSprite(50, 400, 100, 100, 40); //(x, y, w, h)
+    pipes = new Group();
+
+
 }
 
 function draw() {
     // background(50, 205, 0)
     background(imgImg)
-
+    //set screen at -100 pixels to start game
+    translate(100, 0)
     // below used to move Helo
     if (keyDown(32) && player.position.y < height) {
         player.position.y = player.position.y - 15;
     }
-    //below sets pace for Helo movement
-    player.position.y = player.position.y + 10
-    player.position.x = player.position.x + 5
 
+    // set pace for walls
+
+    //below sets pace for Helo movement
+    player.position.y = player.position.y + 8
+    player.position.x = player.position.x + 0
+
+
+
+    //needed to render sprites
     drawSprites()
 }
-
-
 
 
 
