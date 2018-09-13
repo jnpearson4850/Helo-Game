ce25f11577d162ced2dc20fae3ab765c92756028
Joshua Nicholas Pearson <jnpearson4850@gmail.com>
2018-09-13 17:51:34 -0400

    just need to deploy

--- sketch.js
+++ sketch.js
@@ -1,31 +1,27 @@
-
 var player;
-// background image
-var img;
-
 var enemy;
 var enemy2;
 var enemy3;
 var enemy4;
-
+var enemy5;
+var enemy6;
+var enemy7;
 var isGameOver
+var isNextLevel
 
-//timer
 function preload() {
-    imgImg = loadImage("https://i.imgur.com/aKQOg3G.png");
+
 }
 function setup() {
-    createCanvas(1000, 700);
+    createCanvas(windowWidth, windowHeight);
+    isNextLevel = false;
     isGameOver = false;
     // player sprite
-    player = createSprite(50, 400, 100, 100, 40); //(x, y, w, h)
-    //top wall sprites
-    wall = createSprite(200, 50, 50, 100) //(x, y, w, h)
-    wall1 = createSprite(300, 100, 100, 200)
-    //bottom wall sprites
-    tri1 = createSprite(200, 700, 100, 200)
-    tri2 = createSprite(300, 700, 100, 450)
-    //flying objects
+    player = createSprite(width / 2, 700, 50, 100, 40); //(x, y, w, h)
+    //goal line for next level
+    goalLine = createSprite(0, 50, 10000, 5)
+
+    //objects
     enemy = createSprite(width / 2, 0, 20, 60);
     enemy.rotationSpeed = 4.0
     enemy2 = createSprite(width / 2, 0, 60, 20);
@@ -33,13 +29,23 @@ function setup() {
     enemy3 = createSprite(width / 2, 0, 40, 20);
     enemy3.rotationSpeed = 6.0
     enemy4 = createSprite(width / 2, 0, 10, 30);
+    enemy4.rotationSpeed = 6.0
+    enemy5 = createSprite(width / 2, 0, 10, 30);
+    enemy5.rotationSpeed = 1.0
+    enemy6 = createSprite(width / 2, 0, 10, 30);
+    enemy6.rotationSpeed = 10.0
+    enemy7 = createSprite(width / 2, 0, 10, 30);
+
 }
 
-function draw() {
-    background(50, 205, 0)
 
+function draw() {
+    background(0, 0, 0, 15);
     if (isGameOver) {
         gameOver()
+    }
+    if (isNextLevel) {
+        nextLevel()
     } else {
         if (enemy.overlap(player)) {
             isGameOver = true;
@@ -53,28 +59,32 @@ function draw() {
         if (enemy4.overlap(player)) {
             isGameOver = true;
         }
-        if (wall.overlap(player)) {
+        if (enemy5.overlap(player)) {
             isGameOver = true;
         }
-        if (wall1.overlap(player)) {
+        if (enemy6.overlap(player)) {
             isGameOver = true;
         }
-        if (tri1.overlap(player)) {
+        if (enemy7.overlap(player)) {
             isGameOver = true;
         }
-        if (tri2.overlap(player)) {
-            isGameOver = true;
+        if (goalLine.overlap(player)) {
+            isNextLevel = true
         }
-
-        // background(0, 0, 100)//RGB attributes for the color of the background
-        // background(backgroundImage)
-        //adding ability to move the sprite, will go back and add up down as well
+        //ability to move the player
         if (keyDown(RIGHT_ARROW) && player.position.x < (width - 25)) {
             player.position.x = player.position.x + 5;
         }
         if (keyDown(LEFT_ARROW) && player.position.x > 25) {
             player.position.x = player.position.x - 5;
         }
+        if (keyDown(40) && player.position.x > 25) {
+            player.position.y = player.position.y + 5;
+        }
+        if (keyDown(38) && player.position.x > 25) {
+            player.position.y = player.position.y - 5;
+        }
+        //below makes the wall and enemys move
         enemy.position.y = enemy.position.y + 10;
         if (enemy.position.y > height) {
             enemy.position.y = 0;
@@ -95,81 +105,73 @@ function draw() {
             enemy4.position.y = 0;
             enemy4.position.x = random(20, width - 2)
         }
-        wall.position.y = wall.position.y - 3
-        if (wall.position.y > height) {
-            wall.position.y = 0;
-            wall.position.y = random(20, width - 2)
+        enemy5.position.y = enemy5.position.y + 10;
+        if (enemy5.position.y > height) {
+            enemy5.position.y = 0;
+            enemy5.position.x = random(18, width - 9)
         }
-        wall1.position.y = wall1.position.y - 4
-        if (wall1.position.y > height) {
-            wall1.position.y = 0;
-            wall1.position.y = random(20, width - 2)
+        enemy6.position.y = enemy6.position.y + 22;
+        if (enemy6.position.y > height) {
+            enemy6.position.y = 0;
+            enemy6.position.x = random(11, width - 5)
         }
-        tri1.position.y = tri1.position.y - 4
-        if (tri1.position.y > height) {
-            tri1.position.y = 0;
-            tri1.position.y = random(20, width - 2)
+        enemy7.position.y = enemy7.position.y + 6;
+        if (enemy7.position.y > height) {
+            enemy7.position.y = 0;
+            enemy7.position.x = random(2, width - 2)
         }
-        tri2.position.y = tri2.position.y - 4
-        if (tri2.position.y > height) {
-            tri2.position.y = 0;
-            tri2.position.y = random(20, width - 2)
-        }
-        // background(imgImg)
         //set screen at -100 pixels to start game
-        translate(100, 0)
-
-        // below used to move Helo(up, right, left)
-        if (keyDown(32) && player.position.y < height) {
-            player.position.y = player.position.y - 15;
-        }
-        if (keyDown(39) && player.position.x < height) {
-            player.position.x = player.position.x + .5
-        }
-        if (keyDown(37) && player.position.x < height) {
-            player.position.x = player.position.x - .5
-        }
-
-        // set pace for walls
-        wall.position.x = wall.position.x - 1
-        wall1.position.x = wall1.position.x - 1
-
-        tri1.position.x = tri1.position.x - 1
-        tri2.position.x = tri2.position.x - 1
-
-        //below sets pace for Helo movement
-        player.position.y = player.position.y + 8
-        player.position.x = player.position.x + 0
-
-        //set objects moving at player
-
         //needed to render sprites
         drawSprites()
     }
+}
+function nextLevel() {
+    background(0)
+    textAlign(CENTER)
+    fill("white")
+    text("Great Job!", width / 2, height / 2)
+    text("Next Level is about to begin!! Click anywhere", width / 2, 3 * height / 4)
+}
 
+function gameOver() {
+    background(0)
+    textAlign(CENTER)
+    fill("white")
+    text("Game Over!", width / 2, height / 2)
+    text("Good Job. But you gotta be quicker!! Click refresh to play again", width / 2, 3 * height / 4)
+}
+// reset game
+function mouseClicked() {
+    if (isNextLevel) {
+        isNextLevel = false;
+        player.position.x = width / 2
+        player.position.y = 700
+        enemy.position.x = width / 2
+        enemy.position.y = 0
+        enemy2.position.x = width / 4
+        enemy2.position.y = -90
+        enemy3.position.x = width / 6
+        enemy3.position.y = 25
+        enemy4.position.x = width / 8
+        enemy4.position.y = -20
 
-
-    function gameOver() {
-        background(0)
-        textAlign(CENTER)
-        fill("white")
-        text("Game Over!", width / 2, height / 2)
-        text("Enter you name to be on the Scoreboard, and click anywhere to play again", width / 2, 3 * height / 4)
-    }
-    // reset game
-    function mouseClicked() {
-        if (isGameOver) {
-            isGameOver = false;
-            player.position.x = width / 2
-            player.position.y = height - 25
-            enemy.position.x = width / 2
-            enemy.position.y = 0
-            enemy2.position.x = width / 4
-            enemy2.position.y = -20
-            enemy3.position.x = width / 6
-            enemy3.position.y = 10
-            enemy4.position.x = width / 8
-            enemy4.position.y = -45
-        }
     }
 }
+// function mouseClicked() {
+//     if (isGameOver) {
+//         isGameOver = false;
+//         player.position.x = 400
+//         player.position.y = 605
+//         enemy.position.x = width / 2
+//         enemy.position.y = 0
+//         enemy2.position.x = width / 4
+//         enemy2.position.y = -20
+//         enemy3.position.x = width / 6
+//         enemy3.position.y = 10
+//         enemy4.position.x = width / 8
+//         enemy4.position.y = -45
+
+//     }
+// }
+
+
